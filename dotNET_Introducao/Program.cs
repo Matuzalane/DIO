b10b90807780300368e4f14187f34114d856ba10
Matuzalane <calebcastilhonunes@gmail.com>
2022-12-07 11:12:47 -0300

    Conclusão do módulo Estruturas de Repetição em C#

--- dotNET_Introducao/Program.cs
+++ dotNET_Introducao/Program.cs
@@ -207,6 +207,80 @@
     }
 
     // WHILE
+    int count = 0;
+
+    while (count <= 10)
+    {
+        Console.WriteLine($"{num} x {count} = {num * count}");
+        count++;
+
+        if (count == 6)
+        {
+            break;
+        }
+    }
+
+    // DO WHILE
+    int sum = 0, number = 0;
+
+    do 
+    {
+        Console.WriteLine("Enter a number (0 to stop)");
+        number = Convert.ToInt32(Console.ReadLine());
+
+        sum += number;
+
+    } while(number != 0);
+
+    Console.WriteLine($"Total sum of numbers entered is: {sum}");
+
+    // Interactive Menu Whith WHILE
+    string option;
+    bool displayMenu = true;
+
+    while(displayMenu)
+    {
+        Console.Clear();
+        Console.WriteLine("Enter your option:");
+        Console.WriteLine("1 - Register Customer");
+        Console.WriteLine("2 - Search Customer");
+        Console.WriteLine("3 - Delete Client");
+        Console.WriteLine("4 - Close");
+
+        option = Console.ReadLine();
+
+        switch(option)
+        {
+            case "1":
+                Console.WriteLine("Customer Registration");
+                break;
+            
+            case "2":
+                Console.WriteLine("Customer Search");
+                break;
+            
+            case "3":
+                Console.WriteLine("Delete Client");
+                break;
+            
+            case "4":
+                Console.WriteLine("Close");
+                //Environment.Exit(0);
+                displayMenu = false;
+                break;
+            
+            default:
+                Console.WriteLine("Invalid Option");
+                break;
+        }
+    }
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ //
+
+// ----------------------------------------------------------- //
+// ------ Structure of a Program and Main Method in C# ------- // 
+// ----------------------------------------------------------- //
+
     
 
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ //
