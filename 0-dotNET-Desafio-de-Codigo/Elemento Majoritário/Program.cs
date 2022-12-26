437f089169059753e22daffeb14da2794cd56d14
Matuzalane <calebcastilhonunes@gmail.com>
2022-12-26 17:51:48 -0300

    Conclusão dos desafios de código intermediários.

---
+++ 0-dotNET-Desafio-de-Codigo/Elemento Majoritário/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        
+        int[] num = new int[n];
+    
+// TODO: Crie as outras condições necessárias para a resolução do desafio:
+        for ( int i = 0; i < num.Length; i++ )
+        {
+            num[i] = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine(MajorityElement(num, num.Length));
+        
+    }
+    public static int MajorityElement(int[] nums, int j)
+    {
+        int major = nums[0];
+        int count = 0;
+        
+        for (int i = 0; i < j; i++ )
+        {
+            if (count == 0)
+            {
+                major = nums[i];
+                count++;
+            }
+            else
+            {
+                if (major == nums[i])
+                {
+                    count++;
+                }
+                else
+                {
+                    count--;
+                }
+            }
+        }
+        return major;
+    }
+}
