347d5c14374282c9388b20467682372cb38f9f06
Matuzalane <calebcastilhonunes@gmail.com>
2023-01-03 11:15:48 -0300

    Conclusão do Módulo: Introdução a APIs com C#

---
+++ 4-dotNET-API-com-Csharp/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _4_dotNET_API_com_Csharp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        [HttpGet("GetCurretntDateTime")]
+        public IActionResult GetDateTime()
+        {
+            var obj = new
+            {
+                Date = DateTime.Now.ToLongDateString(),
+                Time = DateTime.Now.ToShortTimeString()
+            };
+
+            return Ok(obj);
+        }
+
+        [HttpGet("Introduce/{name}")]
+        public IActionResult Introduce(string name)
+        {
+            var mensage = $"Hello {name}, be welcome!";
+
+            return Ok(new { mensage });
+        }
+    }
+}
